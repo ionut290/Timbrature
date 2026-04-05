46919fdb44bd0308fe66b587513718cab8ccb3d9
ionut290 <148634773+ionut290@users.noreply.github.com>
2026-04-05 10:03:07 +0200

    Fix import quotes in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-// Config Firebase letta da variabili ambiente (Vite)
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
